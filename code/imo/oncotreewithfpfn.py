69ced886a09febd9e80adb6615bfafc57c98064f
Mohammadsadegh Akhondzadeh <ms.akhondzadeh@gmail.com>
2018-05-29 22:56:06 +0430

    Add fit function

--- code/imo/oncotreewithfpfn.py
+++ code/imo/oncotreewithfpfn.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import networkx as nx
-
-
+import matplotlib.pyplot as plt
 class OncoTreeWithFpFn(object):
     """
     The tree model for oncogenesis by Szabo, Aniko, et al.
@@ -22,7 +21,7 @@ class OncoTreeWithFpFn(object):
 
     def __init__(self, min_mutations=None):
         self.edge_list = None
-        self.optimum_branching = None
+        self.opontimum_branching = None
         self.min_mutations = min_mutations
         self.mutation_freq = None
         self.branching_edge = None
@@ -45,8 +44,12 @@ class OncoTreeWithFpFn(object):
                                                        , axis=1)
         self.edge_list.drop(['case_id_x', 'case_id_y', 'key'], axis=1, inplace=True)
 
+        # compute the number of cases
         n_cases = len(df['case_id'].unique())
         self.edge_list = self.edge_list[self.edge_list['symbol_x'] != self.edge_list['symbol_y']]
+
+        # compute the edge weights of the graph using the formula:
+        # w_ij = log(p_ij) - log(p_i + p_j) - log(p_j)
         self.edge_list['weight'] = self.edge_list.apply(
             lambda row: np.log(row['count'] / n_cases) -
                         np.log(cases.loc[row['symbol_x']]['count'] / n_cases +
@@ -57,6 +60,14 @@ class OncoTreeWithFpFn(object):
         self.mutation_freq = list(cases.sort_values('count')['symbol'])
 
     def find_branching(self):
+
+        """
+
+        find the minimum branching tree by finding the parent of every nodes
+
+
+        :return: optimum branching
+        """
         tree = list()
         all_edge = self.edge_list.copy()
         all_edge.sort_values('weight', ascending=False, inplace=True)
@@ -65,13 +76,42 @@ class OncoTreeWithFpFn(object):
                 tree.append(all_edge[all_edge.symbol_y == i].iloc[0])
                 all_edge = all_edge[all_edge.symbol_x != i]
             except:
-                print("Warning: there is a mutation that have no parent.")
+                print("Warning: there is a mutation that has no parent.")
         self.branching_edge = pd.DataFrame(tree)
         self.optimum_branching = nx.from_pandas_edgelist(self.branching_edge, source='symbol_x', target='symbol_y',
                             edge_attr='weight', create_using=nx.DiGraph())
 
         return self.optimum_branching
 
-    def draw(self):
-        pos = nx.drawing.nx_pydot.pydot_layout(self.optimum_branching, prog='dot')
-        nx.draw(self.optimum_branching, pos, with_labels=True)
+
+    def fit(self, df):
+        """
+        Runs algorithm on the provided data frame.
+
+
+        :param df: pandas.DataFrame
+            The data matrix. Should have ``Hugo_Symbol`` and ``case_id`` columns.
+
+        """
+        self.create_init_graph(df)
+        self.find_branching()
+
+
+    def draw(self, figsize= (30,20), weight = False):
+        """
+        Plot the graph
+
+        :param figsize:
+        :param weight: show the weight of edge
+        """
+        plt.figure(figsize = figsize)
+        if weight == False:
+            pos = nx.drawing.nx_pydot.pydot_layout(self.optimum_branching, prog='dot')
+            nx.draw(self.optimum_branching, pos, with_labels=True)
+        else:
+            pos = nx.drawing.nx_pydot.pydot_layout(self.optimum_branching, prog='dot')
+            nx.draw_networkx_edge_labels(self.optimum_branching, pos,
+                                         edge_labels={(u, v): round(self.optimum_branching.get_edge_data(u, v)['weight'], 2) for u, v in
+                                         self.optimum_branching.edges});
+
+            nx.draw(self.optimum_branching, pos, with_labels=True)
